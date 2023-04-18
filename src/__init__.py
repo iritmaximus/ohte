856b942058443cd361dc2ab7e38d9897ca959d9c
iritmaximus <aaro.koivukangas@gmail.com>
2023-04-18 12:42:36 +0300

    modified import method to more robust

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 """Defines global variable engine for making a connection to db"""
 import sqlalchemy
-import config
+from src import config
 
 engine = sqlalchemy.create_engine(config.db_url(), echo=True)
