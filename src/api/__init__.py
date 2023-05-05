d6f4d7018cec49def771469398511cb35a42ae75
iritmaximus <aaro.koivukangas@gmail.com>
2023-05-05 15:05:06 +0300

    removed search from project, not implementing

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,18 +1,19 @@
+"""Connects all url paths together, implements app variable"""
+
 from fastapi import FastAPI
 
 from src.api.games import games
 from src.api.user import user_api as user
 from src.api.rating import rating
-from src.api.search import search
 
 
 app = FastAPI()
 app.mount("/api/games", games)
 app.mount("/api/users", user)
 app.mount("/api/rating", rating)
-app.mount("/api/search", search)  # TODO possibly
 
 
 @app.get("/")
 async def root():
+    """Test page to greet the user when querying the base url :)"""
     return {"message": "Hello, world!"}
